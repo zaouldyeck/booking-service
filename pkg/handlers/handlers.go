b8c54a1174cf7bd7cbd37241b43bbd764d965762
Paul Ohrt <paul.ohrt@internetstiftelsen.se>
2025-07-22 15:17:42 +0200

    convert html pages to go templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -49,3 +49,18 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// Generals renders the corresponding room page.
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// Majors renders the corresponding room page.
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+// Availability renders the search availability page.
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
